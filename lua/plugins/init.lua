7925210f83176f61cda0eea1b5f104ed68947c71
Nico Bistolfi <git@bistol.fi>
2025-05-06 16:42:11 -0700

    Installing Parrot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,4 +25,35 @@ return {
   		},
   	},
   },
+
+  {
+    "frankroeder/parrot.nvim",
+    lazy = false,
+    dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+    opts = {
+      providers = {
+      anthropic = {
+        api_key = os.getenv("ANTHROPIC_API_KEY"),
+        endpoint = "https://api.anthropic.com/v1/messages",
+        topic_prompt = "You only respond with 3 to 4 words to summarize the past conversation.",
+        topic = {
+          model = "claude-3-haiku-20240307",
+          params = { max_tokens = 32 },
+        },
+        -- default parameters for the actual model
+        params = {
+          chat = { max_tokens = 4096 },
+          command = { max_tokens = 4096 },
+        },
+        -- optionally provide a selection of your favorite models
+        models = {
+          "claude-3-haiku-20240307",
+          "claude-3-opus-20240229",
+          "claude-3-sonnet-20240229",
+        }
+      },
+      }
+    },
+  },
+
 }
